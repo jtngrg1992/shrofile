01484c328e53fcfe739805e451d6a00766bf8310
jatingarg <jtngrg1992@hotmail.com>
2017-06-13 19:30:37 +0530

    Initial Commit

---
+++ shrofile/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  shrofile
+//
+//  Created by Jatin Garg on 13/06/17.
+//  Copyright © 2017 Jatin Garg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
